044301553305f1d78836508d2ff4576b1420f758
dualshotr2 <yunjch21@icloud.com>
2026-09-13 02:31:40 +0900

    Add Vial support for Commission Corner CC TKL

---
+++ keyboards/comission_corner/cc_tkl/keymaps/vial/config.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef MANUFACTURER
+#define MANUFACTURER "DLS"
+
+#undef PRODUCT
+#define PRODUCT "CC TKL"
+
+#define VIAL_KEYBOARD_UID {0x0C, 0x9E, 0x3E, 0xE9, 0xD9, 0x48, 0x15, 0x7A}
+
+// Esc + Enter
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
---
+++ keyboards/comission_corner/cc_tkl/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Disable the two WS2812 LEDs connected to B9 in the Vial firmware.
+RGBLIGHT_ENABLE = no
